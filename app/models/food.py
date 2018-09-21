4feae2d87cc2419dd3df704c18c5dda4861a8f51
d-rita <drnanyanzi@gmail.com>
2018-09-21 16:07:10 +0300

    Adds tests and models

---
+++ app/models/food.py
@@ -0,0 +1,16 @@
+class Food(object):
+    """This class represents an instance of the food available on the menu"""
+
+    def __init__(self,foodId,foodName,foodPrice):
+        self.foodId=foodId
+        self.foodName=foodName
+        self.foodPrice=foodPrice
+
+    def add_food(self):
+        pass
+
+    def delete_food(self):
+        pass
+
+    def update_food(self):
+        pass
