298e2922897874a4c6ac562fea905f3dc6ba25f4
d-rita <drnanyanzi@gmail.com>
2018-09-23 13:31:35 +0300

    Modifies views to get a particular order

--- app/views/views.py
+++ app/views/views.py
@@ -1,32 +1,44 @@
 from flask import Flask, jsonify, request, json, make_response
 from app import app
-from app.models.orders import orders
+from app.models.orders import orders, Orders, generate_orderId
 
 
 
 #GET ALL ORDERS
 @app.route('/api/v1/orders', methods=['GET'])
 def get_all_orders():
-    return make_response(jsonify({'orders': orders}), 200)
+    if orders:
+        return make_response(jsonify({'orders': orders}), 200)
+    else:
+        return make_response(jsonify({'message':'No orders'}), 404)
 
 #FETCH A PARTICULAR ORDER
 @app.route('/api/v1/orders/<int:orderId>', methods=['GET'])
 def get_an_order(orderId):
-        food_order=[order for order in orders if order['orderId']==orderId]
-        return jsonify({'order': food_order[0]})
+    for order in orders:
+        if order['orderId'] == orderId:
+            return make_response(jsonify(order), 200)
+    return make_response(jsonify({'message': 'Order not found'}), 400)
 
 #CREATE A NEW ORDER
 @app.route('/api/v1/orders', methods=['POST'])
 def add_order():
-    order={
-        'name': request.json['name'], 
-        'price': request.json['price'],
-        'location': request.json['location'],
-        'payment':'cash on delivery',
-        'date': request.json['date']
+    name= request.json['name']
+    price= request.json['price']
+    location= request.json['location']
+    payment='cash on delivery'
+    date= request.json['date']
+    order = {
+        'orderId':generate_orderId(orders),
+        'location':location,
+        'name':name,
+        'price':price,
+        'payment':payment,
+        'date':date
     }
+    Orders('orderId', location= location, name = name, price =price, payment = payment, date = date )
     orders.append(order)
-    return make_response(jsonify({'message': "Order added successfully"}), 201)
+    return make_response(jsonify({'message': "Order sent successfully"}), 201)
     
 #Update status of order
 @app.route('/orders/<int:orderId>', methods=['PUT'])
