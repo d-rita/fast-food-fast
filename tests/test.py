e7941ee695d04d39b97a715c32f66506fe400976
d-rita <drnanyanzi@gmail.com>
2018-09-23 19:19:32 +0300

    Adds tests to test update API

--- tests/test.py
+++ tests/test.py
@@ -13,7 +13,7 @@ class BaseTestCase(unittest.TestCase):
     def tearDown(self):
        orders[:]=[]
 
-    def post_order(self, orderId, location, payment,name, price,quantity, date):
+    def post_order(self, orderId, location, payment,name, price,quantity, date, status):
             return self.client.post('/api/v1/orders',
             data=json.dumps(dict( 
                 orderId=orderId,
@@ -22,7 +22,8 @@ class BaseTestCase(unittest.TestCase):
                 name=name,
                 price=price,
                 quantity=quantity,
-                date=date
+                date=date,
+                status=status
             )
             ),
             content_type='application/json'
@@ -34,12 +35,27 @@ class BaseTestCase(unittest.TestCase):
     def get_an_order(self):
         return self.client.get('api/v1/orders/1')
 
+    def edit_order_status(self,orderId, location, payment,name, price,quantity, date, status):
+        return self.client.put('api/v1/orders/1',
+            data=json.dumps(dict(
+                orderId=orderId,
+                location=location, 
+                payment=payment, 
+                name=name,
+                price=price,
+                quantity=quantity,
+                date=date,
+                status=status
+            )
+            ),
+            content_type='application/json'
+            )
 
 class TestOrdersApi(BaseTestCase):
 
     def test_can_get_all_orders_list(self):
         with self.client:
-            self.post_order(1,'Bunga', 'cash','burger','22000',1, '12/02/2018')
+            self.post_order(1,'Bunga', 'cash','burger','22000',1, '12/02/2018', 'Pending')
             response=self.get_orders()
             self.assertEqual(response.status_code, 200)
 
@@ -54,9 +70,9 @@ class TestOrdersApi(BaseTestCase):
             data=json.loads(response.data.decode())
             self.assertEqual(data.get('message'), 'No orders')
 
-    def test_get_existng_order(self):
+    def test_get_existing_order(self):
         with self.client:
-            self.post_order(1,'Bunga', 'cash','burger','22000',1, '12/02/2018')
+            self.post_order(1,'Bunga', 'cash','burger','22000',1, '12/02/2018', 'Pending')
             response=self.get_an_order()
             self.assertEqual(response.status_code, 200)
 
@@ -73,17 +89,40 @@ class TestOrdersApi(BaseTestCase):
    
     def test_can_add_order(self):
         with self.client:
-            response=self.post_order(1,'Bunga', 'cash','burger','22000',1, '12/02/2018')
+            response=self.post_order(1,'Bunga', 'cash','burger','22000',1, '12/02/2018','Pending')
             self.assertEqual(response.status_code, 201)
         
     def test_add_order_message(self):
         with self.client:
-            response=self.post_order(1,'Bunga', 'cash','burger','22000',1, '12/02/2018')
+            response=self.post_order(1,'Bunga', 'cash','burger','22000',1, '12/02/2018', 'Pending')
             data=json.loads(response.data.decode())
-            print(data)
-            print(response)
             self.assertEqual(data.get('message'), "Order sent successfully")
 
+    def test_can_edit_order_status(self):
+        with self.client:
+            self.post_order(1,'Bunga', 'cash','burger','22000',1, '12/02/2018', 'Pending')
+            response=self.edit_order_status(1,'Bunga', 'cash','burger','22000',1, '12/02/2018', 'Completed')
+            self.assertEqual(response.status_code, 200)
+
+    def test_can_edit_order_status_message(self):
+        with self.client:
+            self.post_order(1,'Bunga', 'cash','burger','22000',1, '12/02/2018', 'Pending')
+            response=self.edit_order_status(1,'Bunga', 'cash','burger','22000',1, '12/02/2018', 'Completed')
+            data=json.loads(response.data.decode())
+            self.assertEqual(data.get('message'), 'Order status updated')
+
+    def test_edit_nonexisting_order_status(self):
+        with self.client:
+            response=self.edit_order_status(1,'Bunga', 'cash','burger','22000',1, '12/02/2018', 'Completed')
+            self.assertEqual(response.status_code, 404)
+
+    def test_edit_nonexisting_order_status_message(self):
+        with self.client:
+            response=self.edit_order_status(1,'Bunga', 'cash','burger','22000',1, '12/02/2018', 'Completed')
+            data=json.loads(response.data.decode())
+            self.assertEqual(data.get('message'), 'Order does not exist')
+
+
     def test_order_class(self):
-        order1=Orders(1,'Bunga', 'cash','burger','22000',1, '12/02/2018')
+        order1=Orders(1,'Bunga', 'cash','burger','22000',1, '12/02/2018', 'Pending')
         self.assertTrue(order1)
