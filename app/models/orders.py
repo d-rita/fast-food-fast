bf874852ce7ac209996430acd8ab0581a499de0f
d-rita <drnanyanzi@gmail.com>
2018-09-23 13:29:52 +0300

    Modifies orders model

--- app/models/orders.py
+++ app/models/orders.py
@@ -1,22 +1,26 @@
 from app.models import food
+from random import randint
 orders = []
+
+id = 0
+def generate_orderId(orders):
+    global id
+    if len(orders)==0:
+        id = len(orders)+1
+    else:
+        id = id+1
+    return id
+
 class Orders(object):
-    def __init__(self, location,name, price, payment, date):
-       # self.orderId=orderId
+    def __init__(self, orderId,location,name, price, payment, date):
+        self.orderId=orderId
         self.location=location
         self.name=name
         self.price=price
         self.payment=payment
-        #self.quantity=quantity
         self.date=date
-        #self.food={}
+        self.accepted = False
+        self.approved = False
+        self.rejected = False
 
-    def add_order(self):
-        pass
-
-    def delete_order(self):
-        pass
-
-    def update_order(self):
-        pass
 
