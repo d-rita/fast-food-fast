f1d2189b35163d341899df81a6756579d93f787b
d-rita <drnanyanzi@gmail.com>
2018-09-25 08:34:18 +0300

    Remove unused variables

--- app/models/orders.py
+++ app/models/orders.py
@@ -1,7 +1,6 @@
-import re
-orders = [] 
+orders = []
 
-Id = 0 
+Id=0
 def generate_orderId(orders):# pragma: no cover
     """Function to generate orderId""" 
     global Id
@@ -9,32 +8,14 @@ def generate_orderId(orders):# pragma: no cover
         Id = len(orders)+1
     else:
         Id = Id+1
-    return Id 
-
-def validate_order(name, price, location, payment, quantity):
-    if not name:
-        return False
-    elif not re.search(r'^[a-zA-Z\S][^0-9]+$', name):
-        return False
-    elif price=='':
-        return False
-    elif location=='':
-        return False
-    elif payment=='':
-        return False
-    elif quantity=='':
-        return False
-    else:
-        return True
-
+    return Id
+    
 class Orders(object):
     """Orders module to define an instance of an order"""
-    def __init__(self, orderId,location,name, price, payment,quantity, date, status):
+    def __init__(self, orderId,location,name, price, date, status):
         self.orderId=orderId
         self.location=location
         self.name=name
         self.price=price
-        self.payment=payment
-        self.quantity=quantity
         self.date=date
         self.status=status
