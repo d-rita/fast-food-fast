c9e4c9ef96ea9c8e37c7d3999fb48d79e7c1c3d4
d-rita <drnanyanzi@gmail.com>
2018-10-03 17:30:06 +0300

    feat(user-orders): Add function to query a particular order for a particular user

--- api/models/orders.py
+++ api/models/orders.py
@@ -3,18 +3,16 @@ import psycopg2
 from api.models.db import DatabaseConnection
 class Orders:
     """Orders class defining methods and variables"""
-    def __init__(self, order_id, location, date, status, menu_id, user_id):
-        self.order_id = order_id
+    def __init__(self, location, date, status, menu_id, user_id):
         self.location = location 
         self.date = date
         self.status = status
         self.menu_id = menu_id
         self.user_id  = user_id
 
-    def add_an_order(self):
-        query = '''INSERT INTO orders (order_id, place, today, order_status, menu_id, user_id)
-        VALUES ('{}', '{}', '{}', '{}', '{}', '{}')'''.format(
-            self.order_id,
+    def add_an_order(self, location, date, status, menu_id, user_id):
+        query = '''INSERT INTO orders (place, today, order_status, menu_id, user_id)
+        VALUES ('{}', '{}', '{}', '{}', '{}') RETURNING order_id'''.format(
             self.location,
             self.date,
             self.status,
@@ -24,3 +22,22 @@ class Orders:
         my_db = DatabaseConnection()
         my_db.cur.execute(query)
 
+    @classmethod
+    def get_user_orders(cls, user_id):
+        """Get a particular user's orders"""
+        query = '''SELECT * FROM orders WHERE user_id = {}'''.format(user_id)
+        my_db = DatabaseConnection()
+        my_db.cur.execute(query)
+        orders = my_db.cur.fetchall()
+        order_list = []
+        for order in orders:
+            ords={}
+            ords['order_id'] = order[0]
+            ords['location'] = order[1]
+            ords['date'] = order[2]
+            ords['status'] = order[3]
+            ords['menu_id'] = order[4]
+            ords['user_id'] = order[5]
+            order_list.append(ords)
+        return order_list
+   
