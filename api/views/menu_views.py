47f9913f01ed9dea3de157af02ceb76cdc3b6cbb
d-rita <drnanyanzi@gmail.com>
2018-10-03 14:09:33 +0300

    feat(add_food_API): Add API to add food to menu [Finishes #160939634]

--- api/views/menu_views.py
+++ api/views/menu_views.py
@@ -2,6 +2,7 @@ from flask import jsonify, make_response, request, Blueprint
 from api.models.menu import Menu
 
 from api import app
+from api.models.db import DatabaseConnection
 
 menu_bp = Blueprint('menu_bp', __name__)
 
@@ -11,13 +12,13 @@ def get_menu():
     return jsonify({'message': database})
 
 @menu_bp.route('/menu', methods=['POST'])
-def add_menu_option():
-    # if not request.get_json:
-    #     return jsonify({'message': 'Wrong data format'}), 400
+def add_menu_option():  
     data = request.get_json()
+    if not data:
+        return jsonify({'message': 'Data should be in json format!'}), 400
     food_name = data['name']
     food_price = data['price']
     food = Menu(f_name=food_name, f_price=food_price)
     food.add_food_item(food_name, food_price)
-    return jsonify({'message': 'Order added succesfully'}), 200
+    return jsonify({'message': 'Order successfully added!'}), 200
     
