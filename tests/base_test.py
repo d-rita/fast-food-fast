f48c525d70a2d1b01bb3d746aecdd1518fd522b9
d-rita <drnanyanzi@gmail.com>
2018-10-04 14:03:41 +0300

    Set up tests for menu APIs

--- tests/base_test.py
+++ tests/base_test.py
@@ -1,5 +1,6 @@
 """BaseTest module"""
 import unittest
+import  json
 import psycopg2
 from api import app
 from api.models.db import DatabaseConnection
@@ -14,7 +15,24 @@ class BaseTestCase(unittest.TestCase):
         #self.app = config(TestingConfig)
         self.client = app.test_client(self)
         self.test_db.create_all_tables()
+        #self.test_db.cur.execute('''INSERT INTO menus (food_name, food_price) VALUES ('Pizza', 12000) RETURNING menu_id''')
+        
 
     def tearDown(self):
         self.test_db.delete_all_tables()
+
+    def add_menu(self, menuid, name, price ):
+        """Defines from post method in JSON format"""
+        return self.client.post('/api/v1/menus',
+            data=json.dumps(dict(
+                menuid=menuid,
+                name=name,
+                price=price
+            )
+        ),
+        content_type='application/json')
+
+    def get_menus(self):
+        """Return all menu options"""
+        return self.client.get('api/v1/menus')
 
