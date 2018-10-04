6381abb8c4e4b935b12675929a8c3df2f6256b8d
d-rita <drnanyanzi@gmail.com>
2018-10-04 17:12:36 +0300

    feat(update-status): Implement update order status API functionality [Finishes #160939573]

--- api/models/orders.py
+++ api/models/orders.py
@@ -1,7 +1,11 @@
 """Orders module"""
 import psycopg2
+from flask import jsonify
+
 from api.models.db import DatabaseConnection
 from api.models.menu import Menu
+
+
 class Orders:
     """Orders class defining methods and variables"""
     def __init__(self, location, date, status, menu_id, user_id):
@@ -22,7 +26,25 @@ class Orders:
         )
         my_db = DatabaseConnection()
         my_db.cur.execute(query)
-        
+
+    @classmethod
+    def get_all_orders(cls):
+        """Get a particular user's orders"""
+        query = '''SELECT * FROM orders'''
+        my_db = DatabaseConnection()
+        my_db.cur.execute(query)
+        orders = my_db.cur.fetchall()
+        order_list = []
+        for order in orders:
+            ords={}
+            ords['order_id'] = order[0]
+            ords['location'] = order[1]
+            ords['date'] = order[2]
+            ords['status'] = order[3]
+            ords['menu_id'] = order[4]
+            ords['user_id'] = order[5]
+            order_list.append(ords)
+        return order_list
 
     @classmethod
     def get_user_orders(cls, user_id):
@@ -44,6 +66,45 @@ class Orders:
         return order_list
    
     @classmethod
-    def update_status(cls, order_id, order_status):
-        valid_status = ['Complete', 'Processing', 'Cancelled']
-        query='''UPDATE orders SET order_status={} WHERE order_id = {}'''.format(valid_status, order_id)
+    def update_status(cls, order_id, ord_status):
+        valid_order = cls.get_an_order(order_id)
+        if valid_order is None:
+            return ('Cannot update non-existing order')
+        else:
+            valid_status = ['Complete', 'Processing', 'Cancelled']
+            orders_list=[]
+            if ord_status not in valid_status:
+                return ('Invalid status')
+            else:
+                query='''UPDATE orders SET order_status = %s WHERE order_id = %s RETURNING order_id, place, today, order_status, menu_id, user_id'''
+                my_db = DatabaseConnection()
+                my_db.cur.execute(query, (ord_status, order_id))
+                updated = my_db.cur.fetchone()
+                u_ord = {}
+                u_ord['order_id'] = updated[0]
+                u_ord['location'] = updated[1]
+                u_ord['date'] = updated[2]
+                u_ord['status'] = updated[3]
+                u_ord['menu_id'] = updated[4]
+                u_ord['user_id'] = updated[5]
+                orders_list.append(u_ord)
+            return orders_list
+        #
+
+    @classmethod
+    def get_an_order(cls, order_id):
+        query = '''SELECT * FROM orders WHERE order_id = {}'''.format(order_id)
+        my_db = DatabaseConnection()
+        my_db.cur.execute(query)
+        order = my_db.cur.fetchone()
+        if order:
+            ords={}
+            ords['order_id'] = order[0]
+            ords['location'] = order[1]
+            ords['date'] = order[2]
+            ords['status'] = order[3]
+            ords['menu_id'] = order[4]
+            ords['user_id'] = order[5]
+            return ords
+        return None
+        #return ('Order does not exist!')
--- api/views/order_views.py
+++ api/views/order_views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, jsonify, make_response, request
-
+from api.models.orders import Orders 
 from api import app
 
 order_bp = Blueprint('order_bp', __name__)
@@ -7,12 +7,23 @@ order_bp = Blueprint('order_bp', __name__)
 @order_bp.route('/orders', methods=['GET'])
 def get_orders():
     """Get all orders"""
-    return jsonify({'message': 'All orders are returned!'})
+    all_orders = Orders.get_all_orders()
+    if len(all_orders) == 0:
+        return jsonify({'message': 'There are no orders'}), 404
+    return jsonify({'message': 'All orders are returned!', 'Orders': all_orders}), 200
 
 @order_bp.route('/orders/<int:order_id>', methods=['GET'])
 def get_an_order(order_id):
-    return jsonify({'message': 'One order returned!'})
+    an_order = Orders.get_an_order(order_id)
+    if an_order is None:
+        return jsonify({'message':'Order does not exist'}), 200
+    return jsonify({'Order':an_order }), 200
 
 @order_bp.route('/orders/<int:order_id>', methods=['PUT'])
 def update_order_status(order_id):
-    return jsonify({'message': 'Order status updated.'})
+    data = request.get_json()
+    if not data:
+        return jsonify({'message': 'Data should be in JSON format!'}), 400
+    status = data['order_status']
+    result = Orders.update_status(order_id, status)
+    return jsonify({'updated_order': result})
