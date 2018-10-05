01fd16a7cbd210bdd52247f36ac2d629fa068816
d-rita <drnanyanzi@gmail.com>
2018-10-05 10:42:22 +0300

    chore(Testing endpoints): Authenticate endpoints and test with Postman

--- api/views/menu_views.py
+++ api/views/menu_views.py
@@ -11,20 +11,24 @@ menu_bp = Blueprint('menu_bp', __name__)
 @jwt_required
 def get_menu():
     user = get_jwt_identity()
-
     my_menu = Menu.get_menu()   
     return make_response(jsonify({'message':'Menu successfully returned', 'Menu': my_menu}), 200)
 
 @menu_bp.route('/menu', methods=['POST'])
 @jwt_required
 def add_menu_option():  
-    user = get_jwt_identity()
-    data = request.get_json()
-    if not data:
-        return jsonify({'message': 'Data should be in json format!'}), 400
-    food_name = data['name']
-    food_price = data['price']
-    food = Menu(f_name=food_name, f_price=food_price)
-    food.add_food_item(food_name, food_price)
-    return jsonify({'message': 'Order successfully added!'}), 201
+    logged_in_admin = get_jwt_identity()
+    if logged_in_admin['admin'] == True:
+        try:
+            data = request.get_json()
+            if not data:
+                return jsonify({'message': 'Data should be in json format!'}), 400
+            food_name = data['name']
+            food_price = data['price']
+            food = Menu(f_name=food_name, f_price=food_price)
+            food.add_food_item(food_name, food_price)
+            return jsonify({'message': 'Order successfully added!'}), 201
+        except KeyError:
+            return jsonify({'message':'Fill in all parameters: name and price'})
+    return jsonify({'message':'Only admins allowed'}), 401
     
--- api/views/user_views.py
+++ api/views/user_views.py
@@ -1,36 +1,48 @@
 import datetime
+
 from flask import Blueprint, jsonify, make_response, request
+from flask_jwt_extended import get_jwt_identity, jwt_required
 
 from api import app
-
 from api.models.menu import Menu, get_food_by_id
 from api.models.orders import Orders
 
 user_bp = Blueprint('user_bp', __name__)
 
 
-@user_bp.route('/<int:user_id>/orders', methods=['GET'])####work on this
-def get_user_orders(user_id):
+@user_bp.route('/orders', methods=['GET'])
+@jwt_required
+def get_user_orders():
     """Get a particular user's orders"""
-    my_orders = Orders.get_user_orders(user_id)
-    if len(my_orders)==0:
-        return jsonify({'message': 'You have no order history!'}), 404
-    return jsonify({'message':'This is your order history', 'Your orders':my_orders}), 200
+    logged_in = get_jwt_identity()
+    user_id = logged_in['user_id']
+    if user_id:
+        my_orders = Orders.get_user_orders(user_id)
+        if len(my_orders)==0:
+            return jsonify({'message': 'You have no order history!'}), 404
+        return jsonify({'message':'This is your order history', 'Your orders':my_orders}), 200
+    return jsonify({'message':'Log in to get your orders'}), 401
 
-@user_bp.route('/orders', methods=['POST'])#Validate user ids
+@user_bp.route('/orders', methods=['POST'])
+@jwt_required
 def add_one_user_order():
     """Place an order by user"""
     try:
         data = request.get_json()
         if not data:
             return jsonify({'message': 'Data should be in JSON!'}), 400
+        logged_in = get_jwt_identity()
+        user_id = data['user_id']
         location = data['location']
         date = datetime.datetime.utcnow()
         order_status = 'New'
         menu_id = get_food_by_id(data['food_id'])
-        user_id = data['user_id']
-        my_order = Orders(location=location, date=date, status=order_status, menu_id=menu_id, user_id=user_id)
-        my_order.add_an_order(location, date, order_status, menu_id, user_id)
-        return jsonify({'message': 'Created one food order'})
-    except:
-        return jsonify({'message': 'Error'}), 500
+        if user_id == logged_in['user_id']:
+            if menu_id is None:
+                return jsonify({'message':'Food is not on the menu'}), 404
+            my_order = Orders(location=location, date=date, status=order_status, menu_id=menu_id, user_id=user_id)
+            my_order.add_an_order(location, date, order_status, menu_id, user_id)
+            return jsonify({'message':'Created one food order'})
+        return jsonify({'message':'User must first log in!'}), 401
+    except KeyError:
+        return jsonify({'message': 'Missing parameter: fill in user_id, location and menu_id'})
