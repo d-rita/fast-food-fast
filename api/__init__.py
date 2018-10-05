238735d09e5b2687be123e43f5f73c9eb785f6a1
d-rita <drnanyanzi@gmail.com>
2018-10-05 07:07:04 +0300

    Add JWT manager

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_jwt_extended import JWTManager
+
 
 app = Flask(__name__)
+jwt = JWTManager(app)
+app.config['SECRET_KEY'] = 'welcometomyworld'
 
 from api.views.order_views import order_bp
 app.register_blueprint(order_bp, url_prefix='/api/v1')
@@ -10,6 +14,9 @@ app.register_blueprint(menu_bp, url_prefix='/api/v1')
 
 from api.views.user_views import user_bp
 app.register_blueprint(user_bp, url_prefix='/api/v1/users')
+
+from api.views.auth import auth_bp
+app.register_blueprint(auth_bp, url_prefix='/api/v1/auth' )
 
 @app.route('/home')
 def home():
