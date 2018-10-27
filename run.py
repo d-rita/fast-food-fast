099a9e3dfb1391affc62d777ab09b4d6f57d64df
d-rita <drnanyanzi@gmail.com>
2018-10-27 04:05:36 +0300

    Change environment variable

--- run.py
+++ run.py
@@ -1,9 +1,9 @@
 from api import app
 from api.views import order_views, user_views, menu_views
 from api.models.db import DatabaseConnection
-from config import ProductionConfig
+from config import DevelopmentConfig
 
-app.config.from_object(ProductionConfig)
+app.config.from_object(DevelopmentConfig)
 
 db_conn = DatabaseConnection()
 db_conn.create_all_tables()
